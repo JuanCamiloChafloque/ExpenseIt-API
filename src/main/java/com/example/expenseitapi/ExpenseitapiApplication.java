2dffe94ee8175c182d1c1961e37d0c035299cef4
Camilo <camilochafloque@gmail.com>
2022-05-12 11:27:19 -0500

    Initial project setup with Spring Initializr

---
+++ src/main/java/com/example/expenseitapi/ExpenseitapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.expenseitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseitapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseitapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/expenseitapi/ExpenseitapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expenseitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseitapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
