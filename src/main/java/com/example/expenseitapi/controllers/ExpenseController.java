50aef9307a2724402967408476e1ea558a97a6c2
Camilo <camilochafloque@gmail.com>
2022-05-16 10:22:20 -0500

    Added configurations for MySQL database

---
+++ src/main/java/com/example/expenseitapi/controllers/ExpenseController.java
@@ -0,0 +1,13 @@
+package com.example.expenseitapi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExpenseController {
+    
+    @GetMapping("/expenses")
+    public String getAllExpenses() {
+        return "List";
+    }
+}
