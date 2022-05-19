0f4a624313f305424cc8984d9509fde613f3311b
Camilo <camilochafloque@gmail.com>
2022-05-19 14:12:19 -0500

    Login feature added

--- src/main/java/com/example/expenseitapi/config/WebSecurityConfig.java
+++ src/main/java/com/example/expenseitapi/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import com.example.expenseitapi.security.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -36,5 +37,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
--- src/main/java/com/example/expenseitapi/controllers/AuthController.java
+++ src/main/java/com/example/expenseitapi/controllers/AuthController.java
@@ -2,6 +2,7 @@ package com.example.expenseitapi.controllers;
 
 import javax.validation.Valid;
 
+import com.example.expenseitapi.entities.LoginModel;
 import com.example.expenseitapi.entities.User;
 import com.example.expenseitapi.entities.UserModel;
 import com.example.expenseitapi.services.UserService;
@@ -9,6 +10,10 @@ import com.example.expenseitapi.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +23,15 @@ public class AuthController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authManager;
     
     @PostMapping("/login")
-    public ResponseEntity<String> login() {
-        return new ResponseEntity<>("User is logged in", HttpStatus.OK);
+    public ResponseEntity<HttpStatus> login(@RequestBody LoginModel data) {
+        Authentication authenticate = authManager.authenticate(new UsernamePasswordAuthenticationToken(data.getEmail(), data.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authenticate);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/register")
